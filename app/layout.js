e1df349c6d2211aebb5c5600de153a2b6b274803
rspartan-117 <riteksharmachd@gmail.com>
2025-01-28 21:30:25 +0530

    Changed footer

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
   const footer = (
     <footer className="p-4 sm:p-8 grid place-items-center">
       <Link href={'https://youtu.be/lkjrUW8fI40'} target="_blank" className="">
-        <p className={'text-indigo-500 duration-200 hover:text-white hover:bg-indigo-500  ' + fugaz.className}>Built by Smoljames 💛</p>
+        <p className={'text-indigo-500 duration-200 hover:text-white hover:bg-indigo-500  ' + fugaz.className}>Built in NextJs</p>
       </Link>
     </footer>
   )
